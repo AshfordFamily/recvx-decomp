c9b1f3219744dc1714c11e0701ebe160f2ef3b24
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-08-24 18:07:24 +0200

    update `en26.h`

--- include/ps2/veronica/prog/en26.h
+++ include/ps2/veronica/prog/en26.h
@@ -6,52 +6,59 @@
 
 typedef struct EN26_WPNDAMAGE_WORK
 {
-	int flg;
-	int nm_act;
-	int nm_blood;
-	int cb_act;
-	int cb_blood;
-	int dw_act;
-	int dw_blood;
+    // total size: 0x1C
+    int flg;        // offset 0x0, size 0x4
+    int nm_act;     // offset 0x4, size 0x4
+    int nm_blood;   // offset 0x8, size 0x4
+    int cb_act;     // offset 0xC, size 0x4
+    int cb_blood;   // offset 0x10, size 0x4
+    int dw_act;     // offset 0x14, size 0x4
+    int dw_blood;   // offset 0x18, size 0x4
 } EN26_WPNDAMAGE_WORK;
 
 typedef struct EN26_FIRE_WORK
 {
-	int obj;
-	float size;
-	float len;
-	NJS_POINT3 ofs;
+    // total size: 0x18
+    int obj;        // offset 0x0, size 0x4
+    float size;     // offset 0x4, size 0x4
+    float len;      // offset 0x8, size 0x4
+	NJS_POINT3 ofs; // offset 0xC, size 0xC
 } EN26_FIRE_WORK;
 
 typedef struct EN26_KAMI_WORK
 {
-	int mtn_no;
-	short frm[4];
+    // total size: 0xC
+    int mtn_no;     // offset 0x0, size 0x4
+    short frm[4];   // offset 0x4, size 0x8
 } EN26_KAMI_WORK;
 
 typedef struct EN26_FMTN_WORK
 {
-	int type;
-	int s_frm;
-	int e_frm;
+    // total size: 0xC
+	int type;   // offset 0x0, size 0x4
+	int s_frm;  // offset 0x4, size 0x4
+	int e_frm;  // offset 0x8, size 0x4
 } EN26_FMTN_WORK;
 
 typedef struct EN26_ATB_WORK
 {
-	int frm;
-	unsigned int act;
+    // total size: 0x8
+	int frm;            // offset 0x0, size 0x4
+	unsigned int act;   // offset 0x4, size 0x4
 } EN26_ATB_WORK;
 
 typedef struct EN26_MTN_WORK
 {
-	int no;
-	EN26_FMTN_WORK fmtn[3];
+    // total size: 0x28
+	int no;                 // offset 0x0, size 0x4
+	EN26_FMTN_WORK fmtn[3]; // offset 0x4, size 0x24
 } EN26_MTN_WORK;
 
 typedef struct EN26_MTN2_WORK
 {
-	int no;
-	EN26_ATB_WORK atb[4];
+    // total size: 0x24
+	int no;                 // offset 0x0, size 0x4
+	EN26_ATB_WORK atb[4];   // offset 0x4, size 0x20
 } EN26_MTN2_WORK;
 
 void bhEne26(BH_PWORK* epw);
