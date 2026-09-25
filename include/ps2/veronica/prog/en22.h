0598b302d5b72c08e2abbbd6d7bc39dc96c85de4
Norgesnerd <5824576+Norgesnerd@users.noreply.github.com>
2026-09-25 23:45:51 +0200

    Decompile `en22` (#73)
    * Decompile `bhEne22_DmmyBrain`
    
    * Decompile `bhEne22`
    
    * Decompile `bhEne22_MainLoop`
    
    * Decompile `bhEne22_DmgChk`
    
    * Decompile `bhEne22_ChgDmgMode`
    
    * Decompile `bhEne22_DamageAdd`
    
    * Decompile `bhEne22_CollCheck`
    
    * Decompile `bhEne22_CollCheckWall`
    
    * Decompile `bhEne22_Init`
    
    * Decompile `bhEne22_InitType00`
    
    * Remove non-DWARF var in `bhEne22_ChgDmgMode`
    
    * Decompile `bhEne22_Move`
    
    * Decompile `bhEne22_Nage`
    
    * Decompile `bhEne22_Damage`
    
    * Decompile `bhEne22_Die`
    
    * Decompile `bhEne22_PlyerHitCheck`
    
    * Decompile `bhEne22_EneSearch`
    
    * Decompile `bhEne22_Brain`
    
    * Decompile `bhEne22_Brain00`
    
    * Decompile `bhEne22_Brain01`
    
    * Decompile `bhEne22_Brain02`
    
    * Decompile `bhEne22_Brain04`
    
    * Decompile `bhEne22_MVType00`
    
    * Decompile `bhEne22_MV00`
    
    * Decompile `bhEne22_MV01`
    
    * Decompile `bhEne22_MV02`
    
    * Decompile `bhEne22_MV03`
    
    * Decompile `bhEne22_MV04`
    
    * Decompile `bhEne22_MV05`
    
    * Decompile `bhEne22_MV06`
    
    * Decompile `bhEne22_DGType00`
    
    * Decompile `bhEne22_DG00`
    
    * Decompile `bhEne22_DG01`
    
    * Decompile `bhEne22_DG02`
    
    * Decompile `bhEne22_DDType00`
    
    * Decompile `bhEne22_DD00`
    
    * Decompile `bhEne22_DD01`
    
    * Decompile `bhEne22_PlyDG00`
    
    * Decompile `bhEne22_PlyDG01`
    
    * Decompile `bhEne22_SetMtn`
    
    * Decompile `bhEne22_CheckMtnTbl`
    
    * Decompile `bhEne22_GetTranslateMtn`
    
    * Decompile `bhEne22_SparkEffect`
    
    * Decompile `bhEne22_GetDengekiColorAddr`
    
    * Decompile `bhEne22_SetWaterEffect`
    
    * Decompile `bhEne22_GetAreaNo`
    
    * Decompile `bhEne22_AreaCheck`
    
    * Decompile `bhEne22_SetTrgPos`
    
    * Decompile `bhEne22_SePlay`
    
    * Decompile `bhEne22_SetDengekiEffect`
    
    * Decompile `bhEne22_SetDengekiEffect2`
    
    * Decompile `bhEne22_SetElectricShockEffect`
    
    * Decompile `bhEne22_SetElectricShockEffect2`
    
    * Decompile `bhEne22_SetElectricLightEffect`
    
    * Decompile `bhEne22_SetLight`
    
    * Decompile `bhEne22_CtrLight`
    
    * Decompile `bhEne22_ChgDengekiColor`
    
    * Group unuseds

--- include/ps2/veronica/prog/en22.h
+++ include/ps2/veronica/prog/en22.h
@@ -2,8 +2,46 @@
 #define _EN22_H_
 
 #include "types.h"
+#include "macros.h"
 
-void bhEne22_DmmyBrain();
+typedef struct EN22_ATB_WORK
+{
+    // total size: 0x8
+    int frm;            // offset 0x0, size 0x4
+    unsigned int act;   // offset 0x4, size 0x4
+} EN22_ATB_WORK;
+
+typedef struct EN22_MTN_WORK
+{
+    // total size: 0x24
+    int no;                 // offset 0x0, size 0x4
+    EN22_ATB_WORK atb[4];   // offset 0x4, size 0x20
+} EN22_MTN_WORK;
+
+typedef struct EN22_POINT2_XZ
+{
+    // total size: 0x8
+    float px;   // offset 0x0, size 0x4
+    float pz;   // offset 0x4, size 0x4
+} EN22_POINT2_XZ;
+
+typedef struct EN22_WEFF_WORK
+{
+    // total size: 0xC
+    int mtn;    // offset 0x0, size 0x4
+    int frm;    // offset 0x4, size 0x4
+    int obj;    // offset 0x8, size 0x4
+} EN22_WEFF_WORK, EN22_WSP_WORK;
+
+typedef struct EN22_DEN_WORK
+{
+    // total size: 0x14
+    int obj;        // offset 0x0, size 0x4
+    int wait;       // offset 0x4, size 0x4
+    NJS_POINT3 ofs; // offset 0x8, size 0xC
+} EN22_DEN_WORK;
+
+void bhEne22_DmmyBrain(BH_PWORK* epw);
 void bhEne22(BH_PWORK* epw);
 void bhEne22_MainLoop(BH_PWORK* epw);
 int bhEne22_DmgChk(BH_PWORK* epw);
@@ -12,9 +50,9 @@ void bhEne22_DamageAdd(BH_PWORK* epw);
 void bhEne22_CollCheck(BH_PWORK* epw);
 void bhEne22_CollCheckWall(BH_PWORK* epw);
 void bhEne22_Init(BH_PWORK* epw);
-void bhEne22_InitType00();
+void bhEne22_InitType00(BH_PWORK* epw);
 void bhEne22_Move(BH_PWORK* epw);
-void bhEne22_Nage();
+void bhEne22_Nage(BH_PWORK* epw);
 void bhEne22_Damage(BH_PWORK* epw);
 void bhEne22_Die(BH_PWORK* epw);
 void bhEne22_PlyerHitCheck(BH_PWORK* pl, BH_PWORK* epw);
@@ -28,36 +66,36 @@ void bhEne22_MVType00(BH_PWORK* epw);
 void bhEne22_MV00(BH_PWORK* epw);
 void bhEne22_MV01(BH_PWORK* epw);
 void bhEne22_MV02(BH_PWORK* epw);
-void bhEne22_MV03();
+void bhEne22_MV03(BH_PWORK* epw);
 void bhEne22_MV04(BH_PWORK* epw);
-void bhEne22_MV05();
+void bhEne22_MV05(BH_PWORK* epw);
 void bhEne22_MV06(BH_PWORK* epw);
 void bhEne22_DGType00(BH_PWORK* epw);
 void bhEne22_DG00(BH_PWORK* epw);
 void bhEne22_DG01(BH_PWORK* epw);
-void bhEne22_DG02();
+void bhEne22_DG02(BH_PWORK* epw);
 void bhEne22_DDType00(BH_PWORK* epw);
 void bhEne22_DD00(BH_PWORK* epw);
-void bhEne22_DD01();
+void bhEne22_DD01(BH_PWORK* epw);
 void bhEne22_PlyDG00(BH_PWORK* pl, BH_PWORK* epw);
 void bhEne22_PlyDG01(BH_PWORK* pl, BH_PWORK* epw);
 int bhEne22_SetMtn(BH_PWORK* epw);
 void bhEne22_CheckMtnTbl(BH_PWORK* epw, int frm);
 void bhEne22_GetTranslateMtn(BH_PWORK* epw, int frm);
 void bhEne22_SparkEffect(BH_PWORK* epw, int frm);
-/*unsigned int* bhEne22_GetDengekiColorAddr(npobj* objp, int no);*/
+unsigned int* bhEne22_GetDengekiColorAddr(NJS_CNK_OBJECT* objp, int no);
 void bhEne22_SetWaterEffect(BH_PWORK* epw, int mtn_no, int frm);
 int bhEne22_GetAreaNo(float px, float pz);
 int bhEne22_AreaCheck(float ene_x, float ene_z, float ply_x, float ply_z);
 int bhEne22_SetTrgPos(BH_PWORK* epw);
-/*void bhEne22_SePlay(BH_PWORK* epw, _anon34* ps, int no);
-_anon0* bhEne22_SetDengekiEffect(BH_PWORK* epw, int obj, _anon34* ofs, float size);*/
+void bhEne22_SePlay(BH_PWORK* epw, NJS_POINT3* ps, int no);
+O_WRK* bhEne22_SetDengekiEffect(BH_PWORK* epw, int obj, NJS_POINT3* ofs, float size);
 void bhEne22_SetDengekiEffect2(BH_PWORK* epw);
 void bhEne22_SetElectricShockEffect(BH_PWORK* epw, int type);
 void bhEne22_SetElectricShockEffect2(BH_PWORK* epw);
-/*_anon0* bhEne22_SetElectricLightEffect(BH_PWORK* epw);
-void bhEne22_SetLight(BH_PWORK* epw, int lnk_obj, _anon34* ofs, int type);*/
+O_WRK* bhEne22_SetElectricLightEffect(BH_PWORK* epw);
+void bhEne22_SetLight(BH_PWORK* epw, int lnk_obj, NJS_POINT3* ofs, int type);
 void bhEne22_CtrLight(BH_PWORK* epw);
-/*void bhEne22_ChgDengekiColor(npobj* objp, int no, unsigned int argb);*/
+void bhEne22_ChgDengekiColor(NJS_CNK_OBJECT* objp, int no, unsigned int argb);
 
 #endif
